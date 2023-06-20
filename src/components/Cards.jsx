bce8cc7b7ccadba89c1561dd08e553c191d9e337
Martín <moz1986@gmail.com>
2023-06-20 00:43:28 -0300

    viajes jujuy2

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import CardItem from './CardItem';
+import lugar1 from '../images/capital.jpg'
+import lugar2 from '../images/maymara.jpg'
+import lugar3 from '../images/cuevas.jpg'
+import lugar4 from '../images/salinas.jpg'
+import lugar5 from '../images/vino.jpg'
 import './Cards.css';
 
 
@@ -11,29 +16,29 @@ function Cards() {
             <div className='cards-wrapper'>
                 <ul className='cards-items'>
                     <CardItem 
-                      src='./src/images/capital.jpg'
+                      src={lugar1}
                       text='Descubriendo Jujuy: Explora la riqueza cultural de su capital.'
                       path='/services'
                     />
                     <CardItem 
-                      src='./src/images/maymara.jpg'
+                      src={lugar2}
                       text='Maravillas en Maymara: Ascenso al cerro sagrado que cautiva los sentidos.'
                       path='/services'
                     />
                 </ul>
                 <ul className='cards-items'>
                     <CardItem 
-                      src='./src/images/cuevas.jpg'
+                      src={lugar3}
                       text='Explorando las Cuevas del Inca: Tesoros ocultos en Humahuaca.'
                       path='/services'
                     />
                     <CardItem 
-                      src='./src/images/salinas.jpg'
+                      src={lugar4}
                       text='Entre cristales blancos: Un viaje único a las Salinas de Jujuy.'
                       path='/services'
                     />
                     <CardItem 
-                      src='./src/images/vino.jpg'
+                      src={lugar5}
                       text='El sabor del encanto: Degusta los secretos de Bodega La Selestina.'
                       path='/services'
                     />
